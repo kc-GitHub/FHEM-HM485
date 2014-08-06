27129fb9fc5eb89e4a5edcbc34def45e502cd3c0
Dirk Hoffmann <dirk-hoffmann@telekom.de>
2014-08-07 00:59:36 +0200

    rework device files

--- FHEM/lib/HM485/Devices/hmw_io_12_fm.pm
+++ FHEM/lib/HM485/Devices/hmw_io_12_fm.pm
@@ -227,12 +227,54 @@ our %definition = (
                                           "link" => {
                                                       "address_start" => 0x349,
                                                       "address_step" => 6,
-                                                      "channel_offset" => 0,
+                                                      "channel_param" => "CHANNEL",
                                                       "count" => 30,
-                                                      "peer_address_offset" => 1,
-                                                      "peer_address_size" => 4.0,
-                                                      "peer_channel_offset" => 5,
-                                                      "peer_channel_size" => 1.0,
+                                                      "parameter" => {
+                                                                       "ACTUATOR" => {
+                                                                                       "hidden" => true,
+                                                                                       "logical" => {
+                                                                                                      "type" => "address"
+                                                                                                    },
+                                                                                       "operations" => "none",
+                                                                                       "physical" => [
+                                                                                                       {
+                                                                                                         "address" => {
+                                                                                                                        "index" => 1
+                                                                                                                      },
+                                                                                                         "interface" => "eeprom",
+                                                                                                         "size" => 4.0,
+                                                                                                         "type" => "integer"
+                                                                                                       },
+                                                                                                       {
+                                                                                                         "address" => {
+                                                                                                                        "index" => 5
+                                                                                                                      },
+                                                                                                         "interface" => "eeprom",
+                                                                                                         "size" => 1.0,
+                                                                                                         "type" => "integer"
+                                                                                                       }
+                                                                                                     ]
+                                                                                     },
+                                                                       "CHANNEL" => {
+                                                                                      "hidden" => true,
+                                                                                      "logical" => {
+                                                                                                     "default" => 255,
+                                                                                                     "max" => 255,
+                                                                                                     "min" => 0,
+                                                                                                     "type" => "integer"
+                                                                                                   },
+                                                                                      "operations" => "none",
+                                                                                      "physical" => {
+                                                                                                      "address" => {
+                                                                                                                     "index" => 0
+                                                                                                                   },
+                                                                                                      "interface" => "eeprom",
+                                                                                                      "size" => 1.0,
+                                                                                                      "type" => "integer"
+                                                                                                    }
+                                                                                    }
+                                                                     },
+                                                      "peer_param" => "ACTUATOR",
                                                       "type" => "LINK"
                                                     },
                                           "master" => {
